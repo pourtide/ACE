9da9bc3e2d4f3425c0c2ebce5825f82f7fc4fea2
DarktideLegend <apourman@protonmail.com>
2022-05-11 06:02:55 -0700

    updated: LoadCSVProperties

--- Source/ACE.Server/Managers/PropertyManager.cs
+++ Source/ACE.Server/Managers/PropertyManager.cs
@@ -36,12 +36,11 @@ namespace ACE.Server.Managers
         {
             if (loadDefaultValues)
             {
-
                 DefaultPropertyManager.LoadDefaultProperties();
-                DefaultPropertyManager.LoadCSVProperties();
             }
 
             LoadPropertiesFromDB();
+            LoadCSVProperties();
 
             if (Program.IsRunningInContainer && !GetString("content_folder").Equals("/ace/Content"))
                 ModifyString("content_folder", "/ace/Content");
@@ -51,7 +50,6 @@ namespace ACE.Server.Managers
             _workerThread.AutoReset = true;
             _workerThread.Start();
         }
-
 
         /// <summary>
         /// Loads the variables from the database directly into the cache.
@@ -70,6 +68,50 @@ namespace ACE.Server.Managers
             foreach (var i in DatabaseManager.ShardConfig.GetAllStrings())
                 CachedStringSettings[i.Key] = new ConfigurationEntry<string>(false, i.Value, i.Description);
         }
+        private static List<CSVProperty<T>> GetCSVProperties<T>(string path)
+        {
+            using (var reader = new StreamReader(path))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<CSVProperty<T>>();
+                return records.ToList();
+
+            }
+        }
+        private static void LoadCSVProperties()
+        {
+            string currentDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string boolPath = Path.Combine(currentDirectory, "default_bool_properties.csv");
+            string stringPath = Path.Combine(currentDirectory, "default_string_properties.csv");
+            string longPath = Path.Combine(currentDirectory, "default_long_properties.csv");
+            string doublePath = Path.Combine(currentDirectory, "default_double_properties.csv");
+
+            var boolProperties = GetCSVProperties<bool>(boolPath);
+            var stringProperties = GetCSVProperties<string>(stringPath);
+            var longProperties = GetCSVProperties<long>(longPath);
+            var doubleProperties = GetCSVProperties<double>(doublePath);
+
+            foreach (var item in boolProperties)
+            {
+                PropertyManager.ModifyBool(item.Key, item.Item);
+            }
+
+            foreach (var item in longProperties)
+            {
+                PropertyManager.ModifyLong(item.Key, item.Item);
+            }
+
+            foreach (var item in stringProperties)
+            {
+                PropertyManager.ModifyString(item.Key, item.Item);
+            } 
+
+            foreach (var item in doubleProperties)
+            {
+                PropertyManager.ModifyDouble(item.Key, item.Item);
+            }
+        }
+
 
         /// <summary>
         /// Resyncs the variables with the database manually.
@@ -485,51 +527,6 @@ namespace ACE.Server.Managers
                 tup => tup.Item1,
                 tup => tup.Item2
             ));
-        }
-
-        private static List<CSVProperty<T>> GetCSVProperties<T>(string path)
-        {
-            using (var reader = new StreamReader(path))
-            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-            {
-                var records = csv.GetRecords<CSVProperty<T>>();
-                return records.ToList();
-
-            }
-        }
-
-        public static void LoadCSVProperties()
-        {
-            string currentDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            string boolPath = Path.Combine("default_bool_properties.csv");
-            string stringPath = Path.Combine("default_string_properties.csv");
-            string longPath = Path.Combine("default_long_properties.csv");
-            string doublePath = Path.Combine("default_double_properties.csv");
-
-            var boolProperties = GetCSVProperties<bool>(boolPath);
-            var stringProperties = GetCSVProperties<string>(stringPath);
-            var longProperties = GetCSVProperties<long>(longPath);
-            var doubleProperties = GetCSVProperties<double>(doublePath);
-
-            foreach (var item in boolProperties)
-            {
-                PropertyManager.ModifyBool(item.Key, item.Item);
-            }
-
-            foreach (var item in longProperties)
-            {
-                PropertyManager.ModifyLong(item.Key, item.Item);
-            }
-
-            foreach (var item in stringProperties)
-            {
-                PropertyManager.ModifyString(item.Key, item.Item);
-            } 
-
-            foreach (var item in doubleProperties)
-            {
-                PropertyManager.ModifyDouble(item.Key, item.Item);
-            }
         }
 
         public static void LoadDefaultProperties()
