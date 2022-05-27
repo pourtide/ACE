0a88ba6c18ad0c441f7d186084448e8c690b0038
DarktideLegend <apourman@protonmail.com>
2022-05-27 01:57:55 -0700

    added: player death results in 5% loss of durability

--- Source/ACE.Server/WorldObjects/Player_Death.cs
+++ Source/ACE.Server/WorldObjects/Player_Death.cs
@@ -477,6 +477,29 @@ namespace ACE.Server.WorldObjects
             // get all items in inventory
             var inventory = GetAllPossessions();
 
+            var durableItems = inventory.Where(i => i.Durability.HasValue).ToList();
+            var maxDurablity = (double)PropertyManager.GetLong("max_armor_durability").Item;
+
+            foreach (var item in durableItems)
+            {
+                // player death removes 5% of maximum durability
+                var rate = 0.05;
+                item.Durability -= (int)(rate * maxDurablity);
+                item.LongDesc = $"Durability: {item.Durability} / {maxDurablity}";
+
+                if (item.Durability <= 0)
+                {
+                    item.Durability = 0;
+                    item.LongDesc = $"Durability: {item.Durability} / {maxDurablity} (BROKEN)";
+                    HandleActionPutItemInContainer(item.Guid.Full, Guid.Full, 0);
+                    Session.Network.EnqueueSend(
+                        new GameMessageSystemChat(
+                            $"Your armor item [{item.Name}] has been destroyed in battle. Purchase an Armor Repair Kit from the Quality of Life Vendor in Arwic to repair this item.", ChatMessageType.System));
+
+                    //if durability is less than or equal to zero, damage should result in no protection from this item so we don't add it to the filteredArmorList
+                }
+            }
+
             // exclude pyreals from randomized death item calculation
             inventory = inventory.Where(i => i.WeenieClassId != coinStackWcid).ToList();
 
