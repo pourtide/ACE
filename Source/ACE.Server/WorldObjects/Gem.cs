af1c97c78d497a744af20e61b8580c46cc2a696f
DarktideLegend <apourman@protonmail.com>
2022-06-03 11:30:40 -0700

    updated: portal gems no longer enabled on the server

--- Source/ACE.Server/WorldObjects/Gem.cs
+++ Source/ACE.Server/WorldObjects/Gem.cs
@@ -157,7 +157,10 @@ namespace ACE.Server.WorldObjects
 
                 // TODO: figure this out better
                 if (spell.MetaSpellType == SpellType.PortalSummon)
-                    TryCastSpell(spell, player, this, tryResist: false);
+                {
+                    player.Session.Network.EnqueueSend(new GameMessageSystemChat($"Portal gems are disabled on this server.", ChatMessageType.System));
+                    //TryCastSpell(spell, player, this, tryResist: false);
+                }
                 else if (spell.IsImpenBaneType || spell.IsItemRedirectableType)
                     player.TryCastItemEnchantment_WithRedirects(spell, player, this);
                 else
